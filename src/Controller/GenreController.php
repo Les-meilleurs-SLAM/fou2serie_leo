d4dafe6933b7ebcd272e2698f7e8dc983783a95a
ldelaroche <delaroche.leo@sfr.fr>
2019-10-08 11:54:22 +0200

    t'est pas beau clement

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -25,5 +25,26 @@ class GenreController extends AbstractController
 
         // $entityManager->flush();
         // return new Response("ajout effectué");
+        
+    // /**
+    //  * @Route("/loadGenres", name="loadGenres")
+    //  */
+    // public function loadGenres()
+    // {
+    //     //Liste des genres à ajouter
+    //     $noms = array('action', 'policier', 'aventure');
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     foreach ($noms as $i => $nom) {
+    //         // on crée un tableau avec les objets Genre
+    //         $liste_genre[$i] = new Genre();
+    //         $liste_genre[$i]->setLibelle($nom);
+    //         // On persiste la liste
+    //         $entityManager->persist($liste_genre[$i]);
+    //     }
+    //     // On déclenche l'enregistrement
+    //     $entityManager->flush();
+    //     return new Response("ajout effectué");
+    // }
+        
     }
 }
